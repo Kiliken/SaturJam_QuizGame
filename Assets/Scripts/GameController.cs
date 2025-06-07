02857d7767332f0bef3c503fda716f462e214cb7
Kiliken <francescopaolomariani7@gmail.com>
2025-06-07 15:09:55 +0900

    Game Completed

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,31 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VectorGraphics;
 using Unity.VectorGraphics.Editor;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+
     [SerializeField]
     private SVGImage flagDisplay;
 
+    [SerializeField]
+    private Button[] buttons;
+
     private float timer;
+    private bool correct = true;
+    private int currentIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-        int index = Random.Range(0, Flags.list.Length);
-        flagDisplay.sprite = Resources.Load<Sprite>($"Flags/{Flags.list[index].ToLower()}");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if( Time.time - timer > 2f )
-        {            
-            int index = Random.Range(0, Flags.list.Length);
-            flagDisplay.sprite = Resources.Load<Sprite>($"Flags/{Flags.list[index].ToLower()}");
-            timer = Time.time;
+        if( correct )
+        {
+            currentIndex = UnityEngine.Random.Range(0, Flags.list.Length);
+            flagDisplay.sprite = Resources.Load<Sprite>($"Flags/{Flags.list[currentIndex].ToLower()}");
+
+            foreach ( var button in buttons )
+            {
+                button.gameObject.SetActive(true);
+                button.GetComponentInChildren<TextMeshProUGUI>().text = Flags.list[UnityEngine.Random.Range(0, Flags.list.Length)];
+            }
+
+            buttons[UnityEngine.Random.Range(0, buttons.Length)].GetComponentInChildren<TextMeshProUGUI>().text = Flags.list[currentIndex];
+
+            correct = false;
         }
+    }
+
+    public void CheckButton(int index)
+    {
+        if(buttons[index].GetComponentInChildren<TextMeshProUGUI>().text == Flags.list[currentIndex])
+            correct = true;
+        else 
+            buttons[index].gameObject.SetActive(false);
     }
 }
